f99ebe1abab4a07d2110dbe1bcb6221848e4615e
Charles Kaminer <ckamine@g.clemson.edu>
2016-07-13 17:21:16 -0600

    implement geocoding add geocoding model, service, controller, and view geocoding link from homepage takes current location from geolocation

---
+++ app/models/geocoding.rb
@@ -0,0 +1,12 @@
+class Geocoding < OpenStruct
+
+  def self.service
+    @@service ||= GeocodingService.new
+  end
+
+  def self.retrieve(lat, lng)
+    address = service.get_address(lat, lng)
+    Geocoding.new(address)
+  end
+
+end
