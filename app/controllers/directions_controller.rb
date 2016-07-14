fbcb27cb34ddf64b760d03c8ffdff0b5d10fba6d
Charles Kaminer <ckamine@g.clemson.edu>
2016-07-14 13:50:50 -0600

    implement map visual on directions page use google map embed api to display map along with directions create map model enable embed api

--- app/controllers/directions_controller.rb
+++ app/controllers/directions_controller.rb
@@ -3,6 +3,7 @@ class DirectionsController < ApplicationController
   def show
     lat = Geolocation.latitude
     lng = Geolocation.longitude
+    @map = Map.retrieve(Geocoding.retrieve(lat, lng).results.first[:formatted_address], params[:chipotle])
     @directions = Direction.route(lat, lng, params[:chipotle])[:steps]
   end
 end
